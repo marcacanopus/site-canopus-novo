a30fb286e605c22947f34d9fa63f714816ee2eb4
PabloGMBatista <pablog.germano@gmail.com>
2025-04-23 12:29:40 -0300

    update(): adjust building page

---
+++ src/app/[slug]/buildingTemplate.tsx
@@ -0,0 +1,47 @@
+"use client";
+
+import { useEffect } from "react";
+import building from "@/data/building.json";
+import Banner from "./components/Banner";
+import About from "./components/About";
+import Breadcrumb from "./components/Breadcrumb";
+import Gallery from "./components/Gallery";
+
+const BuildingTemplate = () => {
+  const primaryColor = building.color.primary;
+  const secondaryColor = building.color.secondary;
+
+  useEffect(() => {
+    document.documentElement.style.setProperty(
+      "--primary-color",
+      primaryColor ?? "#000000"
+    );
+    document.documentElement.style.setProperty(
+      "--secondary-color",
+      secondaryColor ?? "#000000"
+    );
+  }, [primaryColor, secondaryColor]);
+
+  return (
+    <>
+      <Banner
+        title={building.title_banner}
+        status={building.status}
+        color={building.color}
+        banner={building.banner}
+      />
+
+      <About
+        name={building.name}
+        logo={building.logo}
+        address={building.address}
+      />
+
+      <Breadcrumb name={building.name} />
+
+      <Gallery />
+    </>
+  );
+};
+
+export default BuildingTemplate;
--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -1,53 +1,9 @@
-"use client";
-
-import { useEffect } from "react";
-import building from "@/data/building.json";
-import Banner from "./components/Banner";
-import About from "./components/About";
-import Breadcrumb from "./components/Breadcrumb";
-import Gallery from "./components/Gallery";
+import BuildingTemplate from "./buildingTemplate";
 
 export function generateStaticParams() {
   return [{}];
 }
 
 export default function Building() {
-  const primaryColor = building.color.primary;
-  const secondaryColor = building.color.secondary;
-
-  // const setBuildingColors = () => {
-
-  // };
-
-  useEffect(() => {
-    document.documentElement.style.setProperty(
-      "--primary-color",
-      primaryColor ?? "#000000"
-    );
-    document.documentElement.style.setProperty(
-      "--secondary-color",
-      secondaryColor ?? "#000000"
-    );
-  }, [primaryColor, secondaryColor]);
-
-  return (
-    <>
-      <Banner
-        title={building.title_banner}
-        status={building.status}
-        color={building.color}
-        banner={building.banner}
-      />
-
-      <About
-        name={building.name}
-        logo={building.logo}
-        address={building.address}
-      />
-
-      <Breadcrumb name={building.name} />
-
-      <Gallery />
-    </>
-  );
+  return <BuildingTemplate />;
 }
