2041e1efa62cba918200ad357e92eabf876d4ddc
PabloGMBatista <pablog.germano@gmail.com>
2025-05-02 16:10:57 -0300

    update(): adjusts on building pgae

--- src/app/[slug]/components/Differentials.tsx
+++ src/app/[slug]/components/Differentials.tsx
@@ -4,7 +4,7 @@ const Differentials = () => {
   return (
     <div className="bg-gray-100 py-20">
       <div className="container mx-auto">
-        <div>
+        <div className="mb-15">
           <h2 className="text-5xl text-[var(--canopusBlue)] font-bold mb-4">
             Diferenciais
           </h2>
@@ -12,8 +12,8 @@ const Differentials = () => {
             Tudo sobre o apartamento
           </p>
         </div>
-        <div className="flex gap-6 text-[var(--buildingText)]">
-          <div className="flex align-center bg-white w-full">
+        <div className="flex flex-wrap gap-x-6 gap-y-4 text-[var(--buildingText)]">
+          <div className="flex align-center bg-white px-4 py-3 w-[calc(50%-(--spacing(6)))]">
             <div className="relative w-[25px] h-[25px] me-2">
               <Image
                 src="/site-canopus-novo/images/icone-local2.svg"
@@ -23,7 +23,7 @@ const Differentials = () => {
             </div>
             <p className="text-sm text-center">Localização privilegiada</p>
           </div>
-          <div className="flex align-center bg-white w-full">
+          <div className="flex align-center bg-white px-4 py-3 w-[calc(50%-(--spacing(6)))]">
             <div className="relative w-[25px] h-[25px] me-2">
               <Image
                 src="/site-canopus-novo/images/icone-ralo.svg"
@@ -32,7 +32,162 @@ const Differentials = () => {
               />
             </div>
             <p className="text-sm text-center">
-              Ralo linear no terraço das unidades dos pavimentos tipo*</p>
+              Ralo linear no terraço das unidades dos pavimentos tipo*
+            </p>
+          </div>
+          <div className="flex align-center bg-white px-4 py-3 w-[calc(50%-(--spacing(6)))]">
+            <div className="relative w-[25px] h-[25px] me-2">
+              <Image
+                src="/site-canopus-novo/images/icone-piso.svg"
+                fill
+                alt="Academia"
+              />
+            </div>
+            <p className="text-sm text-center">
+              Entregue com piso em todo apartamento*
+            </p>
+          </div>
+          <div className="flex align-center bg-white px-4 py-3 w-[calc(50%-(--spacing(6)))]">
+            <div className="relative w-[25px] h-[25px] me-2">
+              <Image
+                src="/site-canopus-novo/images/icone-cama.svg"
+                fill
+                alt="Academia"
+              />
+            </div>
+            <p className="text-sm text-center">
+              Apartamentos com largura para cama queen size*
+            </p>
+          </div>
+          <div className="flex align-center bg-white px-4 py-3 w-[calc(50%-(--spacing(6)))]">
+            <div className="relative w-[25px] h-[25px] me-2">
+              <Image
+                src="/site-canopus-novo/images/icone-porta-inteligente.svg"
+                fill
+                alt="Academia"
+              />
+            </div>
+            <p className="text-sm text-center">Fechadura biométrica*</p>
+          </div>
+          <div className="flex align-center bg-white px-4 py-3 w-[calc(50%-(--spacing(6)))]">
+            <div className="relative w-[25px] h-[25px] me-2">
+              <Image
+                src="/site-canopus-novo/images/icone-marcador.svg"
+                fill
+                alt="Academia"
+              />
+            </div>
+            <p className="text-sm text-center">
+              Medidores de água e gás individualizados*
+            </p>
+          </div>
+          <div className="flex align-center bg-white px-4 py-3 w-[calc(50%-(--spacing(6)))]">
+            <div className="relative w-[25px] h-[25px] me-2">
+              <Image
+                src="/site-canopus-novo/images/icone-ar-condicionado.svg"
+                fill
+                alt="Academia"
+              />
+            </div>
+            <p className="text-sm text-center">
+              Infraestrutura para ar-condicionado*
+            </p>
+          </div>
+          <div className="flex align-center bg-white px-4 py-3 w-[calc(50%-(--spacing(6)))]">
+            <div className="relative w-[25px] h-[25px] me-2">
+              <Image
+                src="/site-canopus-novo/images/icone-dual-flux.svg"
+                fill
+                alt="Academia"
+              />
+            </div>
+            <p className="text-sm text-center">
+              Vasos sanitários com sistema dual flux*
+            </p>
+          </div>
+          <div className="flex align-center bg-white px-4 py-3 w-[calc(50%-(--spacing(6)))]">
+            <div className="relative w-[25px] h-[25px] me-2">
+              <Image
+                src="/site-canopus-novo/images/icone-janela.svg"
+                fill
+                alt="Academia"
+              />
+            </div>
+            <p className="text-sm text-center">
+              Soleiras e pingadeiras em granito*
+            </p>
+          </div>
+          <div className="flex align-center bg-white px-4 py-3 w-[calc(50%-(--spacing(6)))]">
+            <div className="relative w-[25px] h-[25px] me-2">
+              <Image
+                src="/site-canopus-novo/images/icone-torneira.svg"
+                fill
+                alt="Academia"
+              />
+            </div>
+            <p className="text-sm text-center">
+              Louças e metais de primeira linha*
+            </p>
+          </div>
+          <div className="flex align-center bg-white px-4 py-3 w-[calc(50%-(--spacing(6)))]">
+            <div className="relative w-[25px] h-[25px] me-2">
+              <Image
+                src="/site-canopus-novo/images/icone-usb-c.svg"
+                fill
+                alt="Academia"
+              />
+            </div>
+            <p className="text-sm text-center">
+              Tomada USB-C no quarto e na sala*
+            </p>
+          </div>
+          <div className="flex align-center bg-white px-4 py-3 w-[calc(50%-(--spacing(6)))]">
+            <div className="relative w-[25px] h-[25px] me-2">
+              <Image
+                src="/site-canopus-novo/images/icone-janela.svg"
+                fill
+                alt="Academia"
+              />
+            </div>
+            <p className="text-sm text-center">
+              Entregue com persiana de enrolar no dormitório*
+            </p>
+          </div>
+          <div className="flex align-center bg-white px-4 py-3 w-[calc(50%-(--spacing(6)))]">
+            <div className="relative w-[25px] h-[25px] me-2">
+              <Image
+                src="/site-canopus-novo/images/icone-janela-caixilho.svg"
+                fill
+                alt="Academia"
+              />
+            </div>
+            <p className="text-sm text-center">
+              Caixilhos do dormitório principal com bandeira fixa inferior*
+            </p>
+          </div>
+          <div className="flex align-center bg-white px-4 py-3 w-[calc(50%-(--spacing(6)))]">
+            <div className="relative w-[25px] h-[25px] me-2">
+              <Image
+                src="/site-canopus-novo/images/icone-porta.svg"
+                fill
+                alt="Academia"
+              />
+            </div>
+            <p className="text-sm text-center">
+              Portas com borracha amortecedora*
+            </p>
+          </div>
+          <div className="flex align-center bg-white px-4 py-3 w-[calc(50%-(--spacing(6)))]">
+            <div className="relative w-[25px] h-[25px] me-2">
+              <Image
+                src="/site-canopus-novo/images/icone-chuveiro.svg"
+                fill
+                alt="Academia"
+              />
+            </div>
+            <p className="text-sm text-center">
+              Aquecimento à gás nos chuveiros e lavatórios das unidades tipo.*
+            </p>
           </div>
         </div>
       </div>
